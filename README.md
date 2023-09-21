b03ec7f24527fcfba65f2a4be16a507c9c81120e
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2023-09-21 10:28:02 +0200

    AB#68937 First MVP of the auto provision script.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # lti-auto-configuration
-Contains a set of scripts to auto provision LTI tools to Canvas
+This repository contains a set of scripts and utility tools to auto provising LTI tools to Canvas.
+
+We developers spend valuable time setting up LTI 1.3 tools in Canvas, we need to create at least one LTI developer key, in some cases API keys, and register the keys in an LTI auth server. Then add the tool to a subaccount to test it or let the testers play with it. And all of that per project and environment.
+
+The intention of the scripts is provide to developers easier ways to create everything needed in a single run, and being able to delete everything that has been created.
+
+To test it locally, first, install the package globally in your system:
+
+```
+npm i -g
+```
+
+Then you can run the script using:
+```
+npx lti-auto-configuration -h
+```
+
+The -h flag will make the tool to provide information about what commands are supported and what parameters are required.
+
+Example of the create command:
+```
+npx lti-auto-configuration -c create -cs https://your.phantastic.canvas.url -t your_admin_token -l https://the.shiny.lti.server.url -lu your_lti_server_username -lp your_lti_server_password -p https://your.shiny.proxy.server.url -tt "The tool name, AB#12345" -tr "your-custom-registration-id" -a your_testing_subaccount
+```
+This command will run the following actions
+ 1. Creates an LTI developer key.
+ 2. Creates an API developer key.
+ 3. Enables both developer keys.
+ 4. Register both keys in the LTI Auth Server.
+ 5. Adds the external tool to the testing subaccount using the LTI develoker Key from step 1.
+
+Example of the delete command:
+```
+npx lti-auto-configuration -c delete -cs https://your.phantastic.canvas.url -t your_admin_token -l https://the.shiny.lti.server.url -lu your_lti_server_username -lp your_lti_server_password -p https://your.shiny.proxy.server.url -d 122010000000000200
+```
+This command will run the following actions
+ 1. Deletes the LTI developer key 122010000000000200.
+ 2. Gets the LTI registration from the LTI Auth Server.
+ 3. Deletes, if exists, the API developer key if exists in the registration.
+ 4. Deletes the LTI registration from the LTI Auth Server.
+
+# TODO
+ - Many things to do and improve, this is just the first version with many pending stuff.
+ - This is only working for page-design and the banner feature.
+ - Accept the tool URL as parameter because right now page-design is hardcoded.
+ - Accept the JSON templates as parameters and move them to the repositories.
+ - Release NPX tool in NPM to use it across repositories.
+
---
+++ bin/index.js
@@ -0,0 +1,351 @@
+#! /usr/bin/env node
+
+const { program } = require('commander');
+const axios = require('axios');
+const JSONBigInt = require('json-bigint');
+const fs = require('fs');
+
+// Constant variables to be replaced
+const DEV_KEY = 'DEV_KEY';
+const LTI_TOOL_TITLE = 'LTI_TOOL_TITLE';
+const LTI_SERVER_URL = 'LTI_SERVER_URL';
+const PROXY_SERVER_URL = 'PROXY_SERVER_URL';
+const LTI_REGISTRATION_ID = 'LTI_REGISTRATION_ID';
+const CANVAS_URL = 'CANVAS_URL';
+const LTI_DEV_ID = 'LTI_DEV_ID';
+const LTI_DEV_KEY = 'LTI_DEV_KEY';
+const API_DEV_ID = 'API_DEV_ID';
+const API_DEV_KEY = 'API_DEV_KEY';
+const ACCOUNT_ID = 'ACCOUNT_ID';
+
+const PAGE_DESIGN_LTI_KEY_TEMPLATE = 'page-design-ltikey-template.json';
+const PAGE_DESIGN_API_KEY_TEMPLATE = 'page-design-apikey-template.json';
+const PAGE_DESIGN_TOOL_REGISTRATION_TEMPLATE = 'page-design-tool-registration-template.json';
+const CANVAS_DELETE_DEV_KEY_API_URL = '/api/v1/developer_keys/';
+const CANVAS_CREATE_LTI_KEY_API_URL = '/api/lti/accounts/1/developer_keys/tool_configuration';
+const CANVAS_CREATE_API_KEY_API_URL = '/api/v1/accounts/1/developer_keys'
+const CANVAS_ENABLE_DEV_KEY_API_URL = `/api/v1/accounts/1/developer_keys/${DEV_KEY}/developer_key_account_bindings`;
+const CANVAS_ADD_EXTERNAL_TOOL_API_URL = `/api/v1/accounts/${ACCOUNT_ID}/external_tools`;
+const TOOL_SUPPORT_CREATE_API_URL = '/admin/tools/';
+
+// Commands
+const CREATE_COMMAND = "CREATE";
+const DELETE_COMMAND = "DELETE";
+
+program.name('index.js').description('Contains a set of CLI tools to auto-provision LTI tools to Canvas').version('1.0.0');
+program
+  .requiredOption('-c, --command <string>', 'The command, it supports create and delete.')
+  .requiredOption('-cs, --canvasserver <string>', 'The Canvas instance URL')
+  .requiredOption('-t, --token <string>', 'The Canvas token to perform API requests')
+  .requiredOption('-l, --ltiserver <string>', 'The LTI Auth Server URL')
+  .requiredOption('-lu, --ltiuser <string>', 'The LTI Auth Server User')
+  .requiredOption('-lp, --ltipassword <string>', 'The LTI Auth Server Password')
+  .option('-p, --proxyserver <string>', 'The Proxy Server URL')
+  .option('-tt, --tooltitle <string>', 'The LTI tool title')
+  .option('-tr, --toolregistrationid <string>', 'The tool registration id')
+  .option('-a, --accountid <string>', 'The testing account id where the tool will be visible')
+  .option('-d, --developerkey <string>', 'The developer key to be deleted')
+  ;
+
+program.parse();
+const options = program.opts();
+
+const canvasUrl = options.canvasserver;
+const canvasToken = options.token;
+const canvasAccountId = options.accountid;
+const canvasDeveloperKeyToDelete = options.developerkey;
+
+const ltiServerURL = options.ltiserver;
+const proxyServerURL = options.proxyserver;
+const ltiUser = options.ltiuser;
+const ltiPassword = options.ltipassword;
+const ltiRegistrationId = options.toolregistrationid;
+const ltiToolTitle = options.tooltitle;
+
+const command = options.command;
+
+/****************************************************************************************/
+/**************************************Canvas********************************************/
+/****************************************************************************************/
+
+const REQUEST_CONFIG = {
+  headers: { Authorization: `Bearer ${canvasToken}` },
+  // IMPORTANT: The developer key ids are greater than the maximum safe integer, we have to use bigints.
+  // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
+  transformResponse: [data => JSONBigInt.parse(data)]
+};
+
+const getDevKeys = async () => {
+  // Create the developer key
+  return await axios.get(`${canvasUrl}${CANVAS_CREATE_API_KEY_API_URL}`, REQUEST_CONFIG)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      throw new Error(`Error getting developer keys ${error}`);
+    });
+}
+
+const deleteDeveloperKeyById = async (developerKeyId) => {
+  // Delete the developer key
+  return await axios.delete(`${canvasUrl}${CANVAS_DELETE_DEV_KEY_API_URL}${developerKeyId}`, REQUEST_CONFIG)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      throw new Error(`Error removing developer key ${developerKeyId} by id ${error}`);
+    });
+}
+
+// Method to create an LTI developer key
+const createLtiDeveloperKey = async () => {
+
+  // Open the template file and replace the variables
+  const pageDesignTemplate = fs.readFileSync(PAGE_DESIGN_LTI_KEY_TEMPLATE, 'utf8');
+  let pageDesignKeyJson = pageDesignTemplate.replaceAll(LTI_TOOL_TITLE, ltiToolTitle);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(LTI_SERVER_URL, ltiServerURL);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(LTI_REGISTRATION_ID, ltiRegistrationId);
+  const developerKeyBody = JSON.parse(pageDesignKeyJson);
+
+  // Create the developer key
+  return await axios.post(`${canvasUrl}${CANVAS_CREATE_LTI_KEY_API_URL}`, developerKeyBody, REQUEST_CONFIG)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      throw new Error(`Error creating the LTI developer key ${error}`);
+    });
+
+   return null;
+}
+
+// Method to create an API developer key
+const createApiDeveloperKey = async () => {
+
+  // Open the template file and replace the variables
+  const pageDesignTemplate = fs.readFileSync(PAGE_DESIGN_API_KEY_TEMPLATE, 'utf8');
+  let pageDesignKeyJson = pageDesignTemplate.replaceAll(LTI_TOOL_TITLE, ltiToolTitle);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(PROXY_SERVER_URL, proxyServerURL);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(LTI_REGISTRATION_ID, ltiRegistrationId);
+  const developerKeyBody = JSON.parse(pageDesignKeyJson);
+
+  // Create the developer key
+  return await axios.post(`${canvasUrl}${CANVAS_CREATE_API_KEY_API_URL}`, developerKeyBody, REQUEST_CONFIG)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      throw new Error(`Error creating the API developer key ${error}`);
+    });
+
+   return null;
+}
+
+// Method to enable a developer key
+const enableDeveloperKey = async (developerKeyId) => {
+
+  const enableDevKeyApiUrl = CANVAS_ENABLE_DEV_KEY_API_URL.replace(DEV_KEY, developerKeyId);
+
+  // The request body to enable a Developer Key
+  const enableDevKeyBodyObject = {
+    developer_key_account_binding: {
+      workflow_state:"on"
+    }
+  };
+
+  // Enable the developer key
+  return await axios.post(`${canvasUrl}${enableDevKeyApiUrl}`, enableDevKeyBodyObject, REQUEST_CONFIG)
+  .then(function (response) {
+    return response.data;
+  })
+  .catch(function (error) {
+    throw new Error(`Error enabling the developer key ${error}`);
+  });
+  return null;
+}
+
+// Method to add the created LTI tool to the testing subaccount.
+const addLtiToolToTestingSubaccount = async (developerKeyId) => {
+
+  const addLtiToolUrl = CANVAS_ADD_EXTERNAL_TOOL_API_URL.replace(ACCOUNT_ID, canvasAccountId);
+
+  // The request body to enable a Developer Key
+  const enableDevKeyBodyObject = {
+    client_id: developerKeyId
+  };
+
+  // Add the external tool to the testing subaccount
+  return await axios.post(`${canvasUrl}${addLtiToolUrl}`, enableDevKeyBodyObject, REQUEST_CONFIG)
+  .then(function (response) {
+    return response.data;
+  })
+  .catch(function (error) {
+    throw new Error(`Error adding the LTI tool ${developerKeyId} to the testing subaccount ${canvasAccountId} ${error}`);
+  });
+  return null;
+}
+
+/****************************************************************************************/
+/**************************************tool-support**************************************/
+/****************************************************************************************/
+const AUTH_CONFIG = {
+  auth: {
+    username: ltiUser,
+    password: ltiPassword
+  }
+}
+
+// Method to create an LTI tool registration in tool-support
+const createLtiToolRegistration = async (ltiDevId, ltiDevApiKey, apiDevId, apiDevApiKey) => {
+
+  // Open the template file and replace the variables
+  const pageDesignTemplate = fs.readFileSync(PAGE_DESIGN_TOOL_REGISTRATION_TEMPLATE, 'utf8');
+  let pageDesignKeyJson = pageDesignTemplate.replaceAll(LTI_TOOL_TITLE, ltiToolTitle);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(LTI_SERVER_URL, ltiServerURL);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(LTI_REGISTRATION_ID, ltiRegistrationId);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(CANVAS_URL, canvasUrl);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(LTI_DEV_ID, ltiDevId);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(LTI_DEV_KEY, ltiDevApiKey);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(API_DEV_ID, apiDevId);
+  pageDesignKeyJson = pageDesignKeyJson.replaceAll(API_DEV_KEY, apiDevApiKey);
+
+  const developerKeyBody = JSON.parse(pageDesignKeyJson);
+
+  // Create the tool registration
+  return await axios.post(`${ltiServerURL}${TOOL_SUPPORT_CREATE_API_URL}`, developerKeyBody, AUTH_CONFIG)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      throw new Error(`Error creating the LTI tool registration ${error}`);
+    });
+
+   return null;
+}
+
+const getLtiToolRegistrationByClientId = async (clientId) => {
+
+  // Get the registration id by clientId
+  return await axios.get(`${ltiServerURL}${TOOL_SUPPORT_CREATE_API_URL}ltiClientId:${clientId}`, AUTH_CONFIG)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      throw new Error(`Error getting the LTI tool registration ${error}`);
+    });
+
+   return null;
+
+}
+
+const deleteLtiToolRegistration = async (registrationId) => {
+
+  // Get the registration id by clientId
+  return await axios.delete(`${ltiServerURL}${TOOL_SUPPORT_CREATE_API_URL}${registrationId}`, AUTH_CONFIG)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      throw new Error(`Error getting the LTI tool registration ${error}`);
+    });
+
+   return null;
+
+}
+
+/****************************************************************************************/
+/**************************************Main Function*************************************/
+/****************************************************************************************/
+
+if (CREATE_COMMAND !== command.toUpperCase() && DELETE_COMMAND !== command.toUpperCase()) {
+  console.error(`Command ${command} is not supported.`);
+  return;
+}
+
+if (CREATE_COMMAND === command.toUpperCase()) {
+
+  if (!proxyServerURL || !ltiRegistrationId || !ltiToolTitle || !canvasAccountId) {
+    console.error('The create command requires the Proxy, RegistrationId, toolTitle and AccountId arguments.');
+    return;
+  }
+
+  (async () => {
+    try {
+
+      const createdLtiDevKey = await createLtiDeveloperKey();
+      const ltiDevId = createdLtiDevKey.developer_key.id.toFixed();
+      const ltiDevApiKey = createdLtiDevKey.developer_key.api_key;
+      console.log(`LTI developer key created with id ${ltiDevId}`);
+
+      const createdApiDevKey = await createApiDeveloperKey();
+      const apiDevId = createdApiDevKey.id.toFixed();
+      const apiDevApiKey = createdApiDevKey.api_key;
+      console.log(`API developer key created with id ${apiDevId}`);
+
+      // Once we have the developer keys, we have to enable them.
+      await enableDeveloperKey(ltiDevId);
+      console.log(`LTI developer key enabled with id ${ltiDevId}`);
+      await enableDeveloperKey(apiDevId);
+      console.log(`API developer key enabled with id ${apiDevId}`);
+
+      // Once the developer keys are enabled we can create the registrations in the LTI auth server.
+      const ltiToolRegistration = await createLtiToolRegistration(ltiDevId, ltiDevApiKey, apiDevId, apiDevApiKey);
+      const ltiToolRegistrationId = ltiToolRegistration.id;
+      console.log(`LTI tool registration created with id ${ltiToolRegistrationId}`);
+
+      // Finally we just need to add the LTI tool to the testing subaccount
+      const externalTool = await addLtiToolToTestingSubaccount(ltiDevId);
+      console.log(`LTI tool with id ${ltiToolRegistrationId} added to the subaccount ${canvasAccountId}. Enjoy your testing.`);
+
+    } catch (error) {
+      console.log(error)
+    }
+  })();
+
+}
+
+if (DELETE_COMMAND === command.toUpperCase()) {
+
+  if (!canvasDeveloperKeyToDelete) {
+    console.error('The delete command requires the Canvas developer key.');
+    return;
+  }
+
+  (async () => {
+    try {
+
+      try {
+        // Delete the developer key from Canvas.
+        await deleteDeveloperKeyById(canvasDeveloperKeyToDelete);
+        console.log(`Developer key ${canvasDeveloperKeyToDelete} deleted successfully.`);
+      } catch (error) {
+        console.log(error);
+      }
+
+      // Search in the LTI auth server for the key to delete it.
+      const ltiToolRegistration = await getLtiToolRegistrationByClientId (canvasDeveloperKeyToDelete);
+      const ltiToolRegistrationId = ltiToolRegistration.id;
+      console.log(`LTI registration found with id ${ltiToolRegistrationId}`);
+      const hasProxyKey = ltiToolRegistration.proxy !== null;
+      if (hasProxyKey) {
+        const canvasApiKeyToDelete = ltiToolRegistration.proxy.clientId;
+        console.log(`The LTI developer key ${canvasDeveloperKeyToDelete} has an API key ${canvasApiKeyToDelete}. Deleting it too....`);
+        try {
+          // Delete the developer key from Canvas.
+          await deleteDeveloperKeyById(canvasApiKeyToDelete);
+          console.log(`Developer key ${canvasApiKeyToDelete} deleted successfully.`);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      // Now delete the LTI tool registration.
+      await deleteLtiToolRegistration(ltiToolRegistrationId);
+      console.log(`LTI registration ${ltiToolRegistrationId} deleted successfully.`);
+
+    } catch (error) {
+      console.log(error)
+    }
+  })();
+
+}
