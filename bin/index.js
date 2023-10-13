67717f021647f480f64a2a7668f94093c8f969ee
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2023-10-13 08:59:33 +0200

    Cleanup the tool origin substitution and fix the page design template.

--- bin/index.js
+++ bin/index.js
@@ -12,7 +12,6 @@ const RELEASE_VERSION = '1.1.6';
 const DEV_KEY = '$DEV_KEY';
 const LTI_TOOL_TITLE = '$LTI_TOOL_TITLE';
 const LTI_TOOL_URL = '$LTI_TOOL_URL';
-const LTI_TOOL_ORIGIN = '$LTI_TOOL_ORIGIN';
 const LTI_SERVER_URL = '$LTI_SERVER_URL';
 const PROXY_SERVER_URL = '$PROXY_SERVER_URL';
 const LTI_REGISTRATION_ID = '$LTI_REGISTRATION_ID';
@@ -128,14 +127,6 @@ if (overridenProperties) {
   })
 }
 
-// To prevent CORS issues we need the tool origin from the URL instead of the tool URL.
-let ltiToolOrigin = '*';
-try {
-  ltiToolOrigin = new URL(ltiToolUrl).origin;
-} catch (error) {
-  console.warn(`The LTI tool url is not valid, ignoring it.`);
-}
-
 let canvasProviderUrl = '';
 // Set the CanvasProviderUrl based on the CanvasUrl
 if (canvasUrl.includes('.test.')) {
@@ -149,8 +140,6 @@ if (canvasUrl.includes('.test.')) {
 // Replace the variables
 jsonTemplate = jsonTemplate.replaceAll(LTI_TOOL_TITLE, ltiToolTitle);
 jsonTemplate = jsonTemplate.replaceAll(LTI_TOOL_URL, ltiToolUrl);
-// The origin shouldn't be needed in the long run as the ltiToolUrl shouldn't contain any path.
-jsonTemplate = jsonTemplate.replaceAll(LTI_TOOL_ORIGIN, ltiToolOrigin);
 jsonTemplate = jsonTemplate.replaceAll(LTI_SERVER_URL, ltiServerURL);
 jsonTemplate = jsonTemplate.replaceAll(PROXY_SERVER_URL, proxyServerURL);
 jsonTemplate = jsonTemplate.replaceAll(LTI_REGISTRATION_ID, ltiRegistrationId);
